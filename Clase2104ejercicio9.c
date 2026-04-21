5da95cfe6433269f9c6128bce41e04fd29f17dde
Fani-malba <fanimalba19@gmail.com>
2026-04-21 14:00:32 -0600

    Add initial version of Clase2104ejercicio9.c
    Initial implementation of a program to read and process 10 positive integers.///// EJERCICIO 9 LUNES 21/04

---
+++ Clase2104ejercicio9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+{
+    void main ()
+    
+      int num, suma=0, promedio=0, mayor=0, menor=0;
+      for(int i = 0; i < 10; i++)
+      {
+          //validar numeros enteros positivos
+          do
+          {
+              printf("Dame un numero entero positivo: ")
+              scanf
+    printf ("Ingrese 10 números enteros positivos  por espacio")
+    
+    Suma, Media
+  
